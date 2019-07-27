e090e921c8aa248c2cedcd5e4efd3e40d01f2a5e
Linda Naughton <lynn@wordsmyth.org>
2019-07-27 15:35:35 -0400

    Fate refresh command wasn't hooked up.

--- plugins/fate/plugin/fate.rb
+++ plugins/fate/plugin/fate.rb
@@ -43,6 +43,8 @@ module AresMUSH
           return FatePointAwardCmd
         when "spend"
           return FatePointSpendCmd
+        when "refresh"
+          return FateRefreshCmd
         end
       when "skill"
         case cmd.switch
