e4bd8769cd66d78e15f525e30112d0dfb8e0f3dc
Linda Naughton <lynn@wordsmyth.org>
2020-02-29 13:15:26 -0500

    Fix profile if traits blank.

--- plugins/traits/plugin/public/traits_api.rb
+++ plugins/traits/plugin/public/traits_api.rb
@@ -10,14 +10,14 @@ module AresMUSH
     end
     
     def self.get_traits_for_web_viewing(char, viewer)
-      char.traits.map { |name, desc| {
+      (char.traits || {}).map { |name, desc| {
         name: name,
         desc: Website.format_markdown_for_html(desc)
       }}
     end
     
     def self.get_traits_for_web_editing(char, viewer)
-      char.traits.map { |name, desc| {
+      (char.traits || {}).map { |name, desc| {
         name: name,
         desc: Website.format_input_for_html(desc)
       }}
